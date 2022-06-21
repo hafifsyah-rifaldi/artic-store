bcdd148fda9497357bd663816fa68f703b8dab71
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-06-21 18:54:06 +0700

    first commit

---
+++ public/script/navbar-scroll.js
@@ -0,0 +1,8 @@
+// Fungsi transition navbar apabila scroll ke content
+
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".navbar-fixed-top");
+    $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+  });
+});
