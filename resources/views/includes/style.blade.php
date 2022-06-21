8e1aba7005ed18de79f0b5648fcd1322d2966e25
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-06-21 21:06:37 +0700

    Konversi Product Page

--- resources/views/includes/style.blade.php
+++ resources/views/includes/style.blade.php
@@ -1,4 +1,4 @@
 <!-- Animasi Website https://michalsnik.github.io/aos/ -->
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
-    <link href="style/main.css" rel="stylesheet" />
+    <link href="/style/main.css" rel="stylesheet" />
     <link rel="shortcut icon" href="/images/shortcut-icon-logo.png">
