707fb9bed93e00c9779eb8d74ab8bef88e699355
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-06-22 19:49:22 +0700

    Fix

--- storage/framework/views/83ec62fe538c51865f7cd6e9ad1e50eb85b42f50.php
+++ storage/framework/views/83ec62fe538c51865f7cd6e9ad1e50eb85b42f50.php
@@ -10,7 +10,7 @@
         <div class="container">
           <div class="row align-items-center row-login justify-content-center">
             <div class="col-lg-6 text-center">
-              <img src="<?php echo e(asset('images/success.svg')); ?>" alt="" class="mb-4" />
+              <img src="/images/success.svg" alt="" class="mb-4" />
               <h2>Transaction Processed!</h2>
               <p>
                 Silahkan tunggu konfirmasi email dari kami dan kami akan
---
+++ storage/framework/views/cfad66960fd7480e3b05cca0ae307af5c9794062.php
@@ -0,0 +1,244 @@
+
+
+<?php $__env->startSection('title'); ?>
+    ARTiC - Categories
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('content'); ?>
+<!-- Page Categories -->
+    <div class="page-content page-home">
+
+      <section class="store-trend-categories">
+        <div class="container">
+          <div class="row">
+            <div class="col-12" data-aos="fade-up">
+              <h5>All Categories</h5>
+            </div>
+          </div>
+          <div class="row">
+            <div
+              class="col-6 offset-md-2 col-md-4 col-lg-2"
+              data-aos="fade-up"
+              data-aos-delay="100"
+            >
+              <a href="#" class="component-categories d-block">
+                <div class="categories-image">
+                  <img
+                    src="/images/categories-artpainting.svg"
+                    alt=""
+                    class="w-100"
+                  />
+                </div>
+                <p class="categories-text">Art Painting</p>
+              </a>
+            </div>
+            <div
+              class="col-6 col-md-4 col-lg-2"
+              data-aos="fade-up"
+              data-aos-delay="200"
+            >
+              <a href="#" class="component-categories d-block">
+                <div class="categories-image">
+                  <img
+                    src="/images/categories-handcraft.svg"
+                    alt=""
+                    class="w-100"
+                  />
+                </div>
+                <p class="categories-text">Handcraft</p>
+              </a>
+            </div>
+            <div
+              class="col-6 col-md-4 col-lg-2"
+              data-aos="fade-up"
+              data-aos-delay="300"
+            >
+              <a href="#" class="component-categories d-block">
+                <div class="categories-image">
+                  <img
+                    src="/images/categories-illustration.svg"
+                    alt=""
+                    class="w-100"
+                  />
+                </div>
+                <p class="categories-text">Illustration</p>
+              </a>
+            </div>
+            <div
+              class="col-6 col-md-4 col-lg-2"
+              data-aos="fade-up"
+              data-aos-delay="400"
+            >
+              <a href="#" class="component-categories d-block">
+                <div class="categories-image">
+                  <img
+                    src="/images/categories-creativeservices.svg"
+                    alt=""
+                    class="w-100"
+                  />
+                </div>
+                <p class="categories-text">Creative Services</p>
+              </a>
+            </div>
+        </div>
+      </section>
+
+      <section class="store-new-products">
+        <div class="container">
+          <div class="row">
+            <div class="col-12" data-aos="fade-up">
+              <h5>All Products</h5>
+            </div>
+          </div>
+
+          <div class="row">
+            <div
+              class="col-6 col-md-4 col-lg-3"
+              data-aos="fade-up"
+              data-aos-delay="100"
+            >
+              <a href="/details.html" class="component-products d-block">
+                <div class="products-thumbnail">
+                  <div
+                    class="products-image"
+                    style="
+                      background-image: url('/images/products-digitalillustration.jpg');
+                    "
+                  ></div>
+                </div>
+                <div class="products-text">Digital Illustration</div>
+                <div class="products-price">Rp. 120.000</div>
+              </a>
+            </div>
+            <div
+              class="col-6 col-md-4 col-lg-3"
+              data-aos="fade-up"
+              data-aos-delay="200"
+            >
+              <a href="/details.html" class="component-products d-block">
+                <div class="products-thumbnail">
+                  <div
+                    class="products-image"
+                    style="
+                      background-image: url('/images/products-illustrationsingle.jpg');
+                    "
+                  ></div>
+                </div>
+                <div class="products-text">Illustration Single</div>
+                <div class="products-price">Rp. 100.000</div>
+              </a>
+            </div>
+            <div
+              class="col-6 col-md-4 col-lg-3"
+              data-aos="fade-up"
+              data-aos-delay="300"
+            >
+              <a href="/details.html" class="component-products d-block">
+                <div class="products-thumbnail">
+                  <div
+                    class="products-image"
+                    style="
+                      background-image: url('/images/products-cartoonillustration.jpg');
+                    "
+                  ></div>
+                </div>
+                <div class="products-text">Cartoon Illustration</div>
+                <div class="products-price">Rp. 120.000</div>
+              </a>
+            </div>
+            <div
+              class="col-6 col-md-4 col-lg-3"
+              data-aos="fade-up"
+              data-aos-delay="400"
+            >
+              <a href="/details.html" class="component-products d-block">
+                <div class="products-thumbnail">
+                  <div
+                    class="products-image"
+                    style="
+                      background-image: url('/images/products-foodart.jpg');
+                    "
+                  ></div>
+                </div>
+                <div class="products-text">Food Art</div>
+                <div class="products-price">Rp. 75.000</div>
+              </a>
+            </div>
+            <div
+              class="col-6 col-md-4 col-lg-3"
+              data-aos="fade-up"
+              data-aos-delay="500"
+            >
+              <a href="/details.html" class="component-products d-block">
+                <div class="products-thumbnail">
+                  <div
+                    class="products-image"
+                    style="
+                      background-image: url('/images/products-birdontree.jpg');
+                    "
+                  ></div>
+                </div>
+                <div class="products-text">Bird on Tree</div>
+                <div class="products-price">Rp. 200.000</div>
+              </a>
+            </div>
+            <div
+              class="col-6 col-md-4 col-lg-3"
+              data-aos="fade-up"
+              data-aos-delay="600"
+            >
+              <a href="/details.html" class="component-products d-block">
+                <div class="products-thumbnail">
+                  <div
+                    class="products-image"
+                    style="
+                      background-image: url('/images/products-petaleddflower.jpg');
+                    "
+                  ></div>
+                </div>
+                <div class="products-text">Petaledd Flower</div>
+                <div class="products-price">Rp. 90.000</div>
+              </a>
+            </div>
+            <div
+              class="col-6 col-md-4 col-lg-3"
+              data-aos="fade-up"
+              data-aos-delay="700"
+            >
+              <a href="/details.html" class="component-products d-block">
+                <div class="products-thumbnail">
+                  <div
+                    class="products-image"
+                    style="
+                      background-image: url('/images/products-macramewall.jpg');
+                    "
+                  ></div>
+                </div>
+                <div class="products-text">Macrame Wall Decoration</div>
+                <div class="products-price">Rp. 105.000</div>
+              </a>
+            </div>
+            <div
+              class="col-6 col-md-4 col-lg-3"
+              data-aos="fade-up"
+              data-aos-delay="800"
+            >
+              <a href="/details.html" class="component-products d-block">
+                <div class="products-thumbnail">
+                  <div
+                    class="products-image"
+                    style="
+                      background-image: url('/images/products-floraltextile.jpg');
+                    "
+                  ></div>
+                </div>
+                <div class="products-text">Floral Textile</div>
+                <div class="products-price">Rp. 60.000</div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\laragon\www\artic-store\resources\views/pages/category.blade.php ENDPATH**/ ?>
