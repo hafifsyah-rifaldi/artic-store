2ebea359c6ede6a612a7496a1adbc50927a4dfad
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-06-22 21:04:33 +0700

    Konversi Dashboard My Product Add Page

---
+++ storage/framework/views/1dbdbd646cd5f3bc82517d5a02ab42afb03001c6.php
@@ -0,0 +1,89 @@
+
+
+<?php $__env->startSection('title'); ?>
+    ARTiC - Dashboard Create Product
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('content'); ?>
+<!-- Section Content -->
+<div
+class="section-content section-dashboard-home"
+data-aos="fade-up"
+>
+<div class="container-fluid">
+    <div class="dashboard-heading">
+    <h2 class="dashboard-title">Create New Product</h2>
+    <p class="dashboard-subtitle">Create your own product</p>
+    </div>
+    <div class="dashboard-content">
+    <div class="row">
+        <div class="col-12">
+        <form action="">
+            <div class="card">
+            <div class="card-body">
+                <div class="row">
+                <div class="col-md-6">
+                    <div class="form-group">
+                    <label>Product Name</label>
+                    <input type="text" class="form-control" />
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="form-group">
+                    <label>Price</label>
+                    <input type="number" class="form-control" />
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <div class="form-group">
+                    <label>Category</label>
+                    <select name="category" class="form-control">
+                        <option value="" disabled>
+                        Select Category
+                        </option>
+                    </select>
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <div class="form-group">
+                    <label>Description</label>
+                    <textarea name="editor"></textarea>
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <div class="form-group">
+                    <label>Thumbnails</label>
+                    <input type="file" class="form-control" />
+                    <p class="text-muted">
+                        Kamu dapat memilih lebih dari satu file
+                    </p>
+                    </div>
+                </div>
+                </div>
+                <div class="row">
+                <div class="col text-right">
+                    <button
+                    type="submit"
+                    class="btn btn-success px-5"
+                    >
+                    Save Now
+                    </button>
+                </div>
+                </div>
+            </div>
+            </div>
+        </form>
+        </div>
+    </div>
+    </div>
+</div>
+</div>
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startPush('addon-script'); ?>
+    <script src="https://cdn.ckeditor.com/4.19.0/standard/ckeditor.js"></script>
+    <script>
+      CKEDITOR.replace("editor");
+    </script>
+<?php $__env->stopPush(); ?>
+<?php echo $__env->make('layouts.dashboard', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\laragon\www\artic-store\resources\views/pages/dashboard-products-create.blade.php ENDPATH**/ ?>
