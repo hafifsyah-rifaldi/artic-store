358614681ea4c66c47add44952cf0c29f86e47be
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-06-23 22:57:33 +0700

    Membuat Halaman Statistik

--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -22,7 +22,7 @@ data-aos="fade-up"
         <div class="card mb-2">
             <div class="card-body">
             <div class="dashboard-card-title">Customer</div>
-            <div class="dashboard-card-subtitle">15</div>
+            <div class="dashboard-card-subtitle"> {{ $customer }} </div>
             </div>
         </div>
         </div>
@@ -30,7 +30,7 @@ data-aos="fade-up"
         <div class="card mb-2">
             <div class="card-body">
             <div class="dashboard-card-title">Revenue</div>
-            <div class="dashboard-card-subtitle">Rp. 450.000</div>
+            <div class="dashboard-card-subtitle">Rp. {{ $revenue }} </div>
             </div>
         </div>
         </div>
@@ -38,7 +38,7 @@ data-aos="fade-up"
         <div class="card mb-2">
             <div class="card-body">
             <div class="dashboard-card-title">Transaction</div>
-            <div class="dashboard-card-subtitle">15</div>
+            <div class="dashboard-card-subtitle">{{ $transaction }} </div>
             </div>
         </div>
         </div>
--- storage/framework/views/58b1f6ea730e49e7c1a53f09d6067670870a8c08.php
+++ storage/framework/views/58b1f6ea730e49e7c1a53f09d6067670870a8c08.php
@@ -22,7 +22,7 @@ data-aos="fade-up"
         <div class="card mb-2">
             <div class="card-body">
             <div class="dashboard-card-title">Customer</div>
-            <div class="dashboard-card-subtitle">15</div>
+            <div class="dashboard-card-subtitle"> <?php echo e($customer); ?> </div>
             </div>
         </div>
         </div>
@@ -30,7 +30,7 @@ data-aos="fade-up"
         <div class="card mb-2">
             <div class="card-body">
             <div class="dashboard-card-title">Revenue</div>
-            <div class="dashboard-card-subtitle">Rp. 450.000</div>
+            <div class="dashboard-card-subtitle">Rp. <?php echo e($revenue); ?> </div>
             </div>
         </div>
         </div>
@@ -38,7 +38,7 @@ data-aos="fade-up"
         <div class="card mb-2">
             <div class="card-body">
             <div class="dashboard-card-title">Transaction</div>
-            <div class="dashboard-card-subtitle">15</div>
+            <div class="dashboard-card-subtitle"><?php echo e($transaction); ?> </div>
             </div>
         </div>
         </div>
