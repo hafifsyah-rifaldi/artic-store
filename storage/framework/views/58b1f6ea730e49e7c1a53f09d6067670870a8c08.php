c1df0a59de58414a793bc7f60de9e6341bd5a6e9
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-06-23 22:27:32 +0700

    Memasang Template Admin

---
+++ storage/framework/views/58b1f6ea730e49e7c1a53f09d6067670870a8c08.php
@@ -0,0 +1,127 @@
+
+
+<?php $__env->startSection('title'); ?>
+    ARTiC - Admin Panel
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('content'); ?>
+<!-- Section Content -->
+<div
+class="section-content section-dashboard-home"
+data-aos="fade-up"
+>
+<div class="container-fluid">
+    <div class="dashboard-heading">
+    <h2 class="dashboard-title">Admin Dashboard</h2>
+    <p class="dashboard-subtitle">This is ARTiC Administrator Panel</p>
+    </div>
+
+    <div class="dashboard-content">
+    <div class="row">
+        <div class="col-md-4">
+        <div class="card mb-2">
+            <div class="card-body">
+            <div class="dashboard-card-title">Customer</div>
+            <div class="dashboard-card-subtitle">15</div>
+            </div>
+        </div>
+        </div>
+        <div class="col-md-4">
+        <div class="card mb-2">
+            <div class="card-body">
+            <div class="dashboard-card-title">Revenue</div>
+            <div class="dashboard-card-subtitle">Rp. 450.000</div>
+            </div>
+        </div>
+        </div>
+        <div class="col-md-4">
+        <div class="card mb-2">
+            <div class="card-body">
+            <div class="dashboard-card-title">Transaction</div>
+            <div class="dashboard-card-subtitle">15</div>
+            </div>
+        </div>
+        </div>
+    </div>
+    <div class="row mt-3">
+        <div class="col-12 mt-2">
+        <h5 class="mb-3">Recent Transactions</h5>
+        <a
+            href="/dashboard-transactions-details.html"
+            class="card card-list d-block"
+        >
+            <div class="card-body">
+            <div class="row">
+                <div class="col-md-1">
+                <img
+                    src="/images/dashboard-icon-product-1.png"
+                    alt=""
+                />
+                </div>
+                <div class="col-md-4">Bird On Tree</div>
+                <div class="col-md-3">Mc Gill</div>
+                <div class="col-md-3">25 Maret, 2022</div>
+                <div class="col-md-1 d-none d-md-block">
+                <img
+                    src="/images/dashboard-arrow-right.svg"
+                    alt=""
+                />
+                </div>
+            </div>
+            </div>
+        </a>
+        <a
+            href="/dashboard-transactions-details.html"
+            class="card card-list d-block"
+        >
+            <div class="card-body">
+            <div class="row">
+                <div class="col-md-1">
+                <img
+                    src="/images/dashboard-icon-product-2.png"
+                    alt=""
+                />
+                </div>
+                <div class="col-md-4">Cartoon Illustration</div>
+                <div class="col-md-3">Salsa Tifani</div>
+                <div class="col-md-3">26 Maret, 2022</div>
+                <div class="col-md-1 d-none d-md-block">
+                <img
+                    src="/images/dashboard-arrow-right.svg"
+                    alt=""
+                />
+                </div>
+            </div>
+            </div>
+        </a>
+        <a
+            href="/dashboard-transactions-details.html"
+            class="card card-list d-block"
+        >
+            <div class="card-body">
+            <div class="row">
+                <div class="col-md-1">
+                <img
+                    src="/images/dashboard-icon-product-3.png"
+                    alt=""
+                />
+                </div>
+                <div class="col-md-4">Digital Illustration</div>
+                <div class="col-md-3">Salsa Tifani</div>
+                <div class="col-md-3">26 Maret, 2022</div>
+                <div class="col-md-1 d-none d-md-block">
+                <img
+                    src="/images/dashboard-arrow-right.svg"
+                    alt=""
+                />
+                </div>
+            </div>
+            </div>
+        </a>
+        </div>
+    </div>
+    </div>
+</div>
+</div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.admin', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\laragon\www\artic-store\resources\views/pages/admin/dashboard.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/5c500ee30a8a5574672e5b060f699c71b369646c.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+
+    <title><?php echo $__env->yieldContent('title'); ?></title>
+
+    <?php echo $__env->yieldPushContent('prepend-style'); ?>
+        <!-- Animasi Website https://michalsnik.github.io/aos/ -->
+        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
+        <link href="/style/main.css" rel="stylesheet" />
+        <link rel="shortcut icon" href="/images/shortcut-icon-logo.png" />
+    <?php echo $__env->yieldPushContent('addon-style'); ?>
+  </head>
+
+  <body>
+    <div class="page-dashboard">
+      <div class="d-flex" id="wrapper" data-aos="fade-right">
+        <!-- Sidebar -->
+        <div class="border-right" id="sidebar-wrapper">
+          <div class="sidebar-heading text-center">
+            <img src="/images/admin.png" alt="" class="my-4" style="max-width: 150px" />
+          </div>
+          <div class="list-group list-group-flush">
+            <a
+              href="/dashboard.html"
+              class="list-group-item list-group-item-action"
+            >
+              Dashboard
+            </a>
+            <a
+              href="#"
+              class="list-group-item list-group-item-action"
+            >
+              Products
+            </a>
+             <a
+              href="#"
+              class="list-group-item list-group-item-action"
+            >
+              Categories
+            </a>
+             <a
+              href="#"
+              class="list-group-item list-group-item-action"
+            >
+              Transactions
+            </a>
+             <a
+              href="#"
+              class="list-group-item list-group-item-action"
+            >
+              Users
+            </a>
+            <a
+              href="/index.html"
+              class="list-group-item list-group-item-action"
+            >
+              Sign Out
+            </a>
+          </div>
+        </div>
+        <!-- /#sidebar-wrapper -->
+
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+          <!-- Navbar -->
+          <nav
+            class="navbar navbar-expand-lg navbar-light navbar-store fixed-top"
+            data-aos="fade-down"
+          >
+            <div class="container-fluid">
+              <button
+                class="btn btn-secondary d-md-none mr-auto mr-2"
+                id="menu-toggle"
+              >
+                &laquo; Menu
+              </button>
+              <button
+                class="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+              >
+                <span class="navbar-toggler-icon"></span>
+              </button>
+
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Desktop Menu -->
+                <ul class="navbar-nav d-none d-lg-flex ml-auto">
+                  <li class="nav-item dropdown">
+                    <a
+                      href="#"
+                      class="nav-link"
+                      id="navbarDropdown"
+                      role="button"
+                      data-toggle="dropdown"
+                    >
+                      <img
+                        src="/images/icon-user.png"
+                        alt=""
+                        class="rounded-circle mr-2 profile-picture"
+                      />
+                      Hi, Hafif
+                    </a>
+                    <div class="dropdown-menu">
+                      <a href="/" class="dropdown-item">Logout</a>
+                    </div>
+                  </li>
+                </ul>
+
+                <ul class="navbar-nav d-block d-lg-none">
+                  <li class="nav-item">
+                    <a href="#" class="nav-link"> Hi, Hafif </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="#" class="nav-link d-inline-block"> Cart </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+
+          
+          <?php echo $__env->yieldContent('content'); ?>
+
+        </div>
+        <!-- /#page-content-wrapper -->
+      </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript -->
+    <?php echo $__env->yieldPushContent('prepend-script'); ?>
+    <script src="/vendor/jquery/jquery.slim.min.js"></script>
+    <script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+      AOS.init();
+    </script>
+    <script>
+      $("#menu-toggle").click(function (e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+      });
+    </script>
+    <?php echo $__env->yieldPushContent('addon-script'); ?>
+  </body>
+</html>
+<?php /**PATH C:\laragon\www\artic-store\resources\views/layouts/admin.blade.php ENDPATH**/ ?>
