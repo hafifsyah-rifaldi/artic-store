b772860869fddd419e1ca6685a56b1d9cc1e42fc
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-06-24 18:51:28 +0700

    Edit / Update

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -108,7 +108,11 @@ class CategoryController extends Controller
      */
     public function edit($id)
     {
-        //
+        $item = Category::findOrFail($id);
+
+        return view('pages.admin.category.edit', [
+            'item' => $item
+        ]);
     }
 
     /**
@@ -120,7 +124,16 @@ class CategoryController extends Controller
      */
     public function update(CategoryRequest $request, $id)
     {
-        //
+        $data = $request->all();
+
+        $data['slug'] = Str::slug($request->name);
+        $data['photo'] = $request->file('photo')->store('assets/category', 'public');
+
+        $item = Category::findOrFail($id);
+
+        $item->update($data);
+
+        return redirect()->route('category.index');
     }
 
     /**
