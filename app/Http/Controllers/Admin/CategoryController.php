1fc8442f90d2b08604e9e36287ee22f7357ba928
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-06-24 18:40:07 +0700

    Create / Save

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -6,6 +6,7 @@ use App\Models\Category;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 use App\Http\Requests\Admin\CategoryRequest;
 
@@ -67,7 +68,7 @@ class CategoryController extends Controller
      */
     public function create()
     {
-        //
+        return view('pages.admin.category.create');
     }
 
     /**
@@ -78,7 +79,14 @@ class CategoryController extends Controller
      */
     public function store(CategoryRequest $request)
     {
-        //
+        $data = $request->all();
+
+        $data['slug'] = Str::slug($request->name);
+        $data['photo'] = $request->file('photo')->store('assets/category', 'public');
+
+        Category::create($data);
+
+        return redirect()->route('category.index');
     }
 
     /**
---
+++ storage/framework/views/6958be5975384112c7e09ee90d67f2ca96076d21.php
@@ -0,0 +1,65 @@
+
+
+<?php $__env->startSection('title'); ?>
+    ARTiC - Admin Category
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('content'); ?>
+<!-- Section Content -->
+<div
+class="section-content section-dashboard-home"
+data-aos="fade-up"
+>
+<div class="container-fluid">
+    <div class="dashboard-heading">
+    <h2 class="dashboard-title">Admin Category</h2>
+    <p class="dashboard-subtitle">Create New Category</p>
+    </div>
+    <div class="dashboard-content">
+        <div class="row">
+            <div class="col-md-12">
+                <?php if($errors->any()): ?>
+                    <div class="alert alert-danger">
+                        <ul>
+                            <?php $__currentLoopData = $errors->all(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $error): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                <li><?php echo e($error); ?></li>
+                            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                        </ul>
+                    </div>
+                <?php endif; ?>
+                <div class="card">
+                    <div class="card-body">
+                        <form action="<?php echo e(route('category.store')); ?>" method="POST" enctype="multipart/form-data">
+                            <?php echo csrf_field(); ?>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label>Category Name</label>
+                                        <input type="text" name="name" class="form-control" required>
+                                    </div>
+                                </div>
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label>Photo</label>
+                                        <input type="file" name="photo" class="form-control" required>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col text-right">
+                                    <button type="submit" class="btn btn-success px-5">
+                                        Save Now
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.admin', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\laragon\www\artic-store\resources\views/pages/admin/category/create.blade.php ENDPATH**/ ?>
