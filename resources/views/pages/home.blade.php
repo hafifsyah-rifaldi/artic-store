56fa68656242b8404ccad5c2512258ba582b14c4
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-06-28 00:32:09 +0700

    Integrasi Landing Page Categories Section

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -8,157 +8,89 @@
     <!-- Page Content navigation 3 slide bootstrap -->
     <div class="page-content page-home">
         <section class="store-carousel">
-        <div class="container">
-            <div class="row">
-            <div class="col-lg-12" data-aos="zoom-in">
-                <div
-                id="storeCarousel"
-                class="carousel slide"
-                data-rides="carousel"
-                >
-                <ol class="carousel-indicators">
-                    <li
-                    class="active"
-                    data-target="#storeCarousel"
-                    data-slide-to="0"
-                    ></li>
-                    <li data-target="#storeCarousel" data-slide-to="1"></li>
-                    <li data-target="#storeCarousel" data-slide-to="2"></li>
-                </ol>
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                    <img
-                        src="/images/banner1.jpg"
-                        alt="Carousel Image"
-                        class="d-block w-100"
-                    />
+            <div class="container">
+                <div class="row">
+                <div class="col-lg-12" data-aos="zoom-in">
+                    <div
+                    id="storeCarousel"
+                    class="carousel slide"
+                    data-rides="carousel"
+                    >
+                    <ol class="carousel-indicators">
+                        <li
+                        class="active"
+                        data-target="#storeCarousel"
+                        data-slide-to="0"
+                        ></li>
+                        <li data-target="#storeCarousel" data-slide-to="1"></li>
+                        <li data-target="#storeCarousel" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                        <img
+                            src="/images/banner1.jpg"
+                            alt="Carousel Image"
+                            class="d-block w-100"
+                        />
+                        </div>
+                        <div class="carousel-item">
+                        <img
+                            src="/images/banner2.jpg"
+                            alt="Carousel Image"
+                            class="d-block w-100"
+                        />
+                        </div>
+                        <div class="carousel-item">
+                        <img
+                            src="/images/banner3.jpg"
+                            alt="Carousel Image"
+                            class="d-block w-100"
+                        />
+                        </div>
                     </div>
-                    <div class="carousel-item">
-                    <img
-                        src="/images/banner2.jpg"
-                        alt="Carousel Image"
-                        class="d-block w-100"
-                    />
-                    </div>
-                    <div class="carousel-item">
-                    <img
-                        src="/images/banner3.jpg"
-                        alt="Carousel Image"
-                        class="d-block w-100"
-                    />
                     </div>
                 </div>
                 </div>
             </div>
-            </div>
-        </div>
         </section>
 
         <section class="store-trend-categories">
-        <div class="container">
-            <div class="row">
-            <div class="col-12" data-aos="fade-up">
-                <h5>Explore Categories</h5>
-            </div>
-            </div>
-            <div class="row">
-            <div
-                class="col-6 col-md-3 col-lg-2"
-                data-aos="fade-up"
-                data-aos-delay="100"
-            >
-                <a href="#" class="component-categories d-block">
-                <div class="categories-image">
-                    <img
-                    src="/images/categories-artpainting.svg"
-                    alt=""
-                    class="w-100"
-                    />
+            <div class="container">
+                <div class="row">
+                    <div class="col-12" data-aos="fade-up">
+                        <h5>Explore Categories</h5>
+                    </div>
                 </div>
-                <p class="categories-text">Art Painting</p>
-                </a>
-            </div>
-            <div
-                class="col-6 col-md-3 col-lg-2"
-                data-aos="fade-up"
-                data-aos-delay="200"
-            >
-                <a href="#" class="component-categories d-block">
-                <div class="categories-image">
-                    <img
-                    src="/images/categories-handcraft.svg"
-                    alt=""
-                    class="w-100"
-                    />
+                <div class="row">
+                    @php $incrementCategory = 0 @endphp
+                    @forelse ($categories as $category)
+                        <div
+                            class="col-6 col-md-3 col-lg-2"
+                            data-aos="fade-up"
+                            data-aos-delay="{{ $incrementCategory+= 100 }}"
+                        >
+                            <a href="{{ route('categories-detail', $category->slug) }}" class="component-categories d-block">
+                                <div class="categories-image">
+                                    <img
+                                    src="{{ Storage::url($category->photo) }}"
+                                    alt=""
+                                    class="w-100"
+                                    />
+                                </div>
+                                <p class="categories-text">
+                                    {{ $category->name }}
+                                </p>
+                            </a>
+                        </div>
+                    @empty
+                        <div class="col-12 text-center py-5" 
+                            data-aos="fade-up"
+                            data-aos-delay="100">
+                            No Categories Found
+                        </div>
+                    @endforelse
                 </div>
-                <p class="categories-text">Handcraft</p>
-                </a>
-            </div>
-            <div
-                class="col-6 col-md-3 col-lg-2"
-                data-aos="fade-up"
-                data-aos-delay="300"
-            >
-                <a href="#" class="component-categories d-block">
-                <div class="categories-image">
-                    <img
-                    src="/images/categories-illustration.svg"
-                    alt=""
-                    class="w-100"
-                    />
-                </div>
-                <p class="categories-text">Illustration</p>
-                </a>
-            </div>
-            <div
-                class="col-6 col-md-3 col-lg-2"
-                data-aos="fade-up"
-                data-aos-delay="400"
-            >
-                <a href="#" class="component-categories d-block">
-                <div class="categories-image">
-                    <img
-                    src="/images/categories-creativeservices.svg"
-                    alt=""
-                    class="w-100"
-                    />
-                </div>
-                <p class="categories-text">Creative Services</p>
-                </a>
-            </div>
-            <div
-                class="col-6 col-md-3 col-lg-2"
-                data-aos="fade-up"
-                data-aos-delay="500"
-            >
-                <a href="#" class="component-categories d-block">
-                <div class="categories-image">
-                    <img
-                    src="/images/categories-creativeservices.svg"
-                    alt=""
-                    class="w-100"
-                    />
-                </div>
-                <p class="categories-text">Creative Services</p>
-                </a>
-            </div>
-            <div
-                class="col-6 col-md-3 col-lg-2"
-                data-aos="fade-up"
-                data-aos-delay="600"
-            >
-                <a href="#" class="component-categories d-block">
-                <div class="categories-image">
-                    <img
-                    src="/images/categories-creativeservices.svg"
-                    alt=""
-                    class="w-100"
-                    />
-                </div>
-                <p class="categories-text">Creative Services</p>
-                </a>
-            </div>
-        </div>
+            </div>    
         </section>
 
         <section class="store-new-products">
