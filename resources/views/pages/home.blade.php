67f0e1309f82affe4fe294daaa0280fa71b0a32b
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-06-28 00:54:02 +0700

    Integrasi Landing Page Categories Section

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -94,160 +94,184 @@
         </section>
 
         <section class="store-new-products">
-        <div class="container">
-            <div class="row">
-            <div class="col-12" data-aos="fade-up">
-                <h5>New Products</h5>
-            </div>
-            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12" data-aos="fade-up">
+                        <h5>New Products</h5>
+                    </div>
+                </div>
 
-            <div class="row">
-            <div
-                class="col-6 col-md-4 col-lg-3"
-                data-aos="fade-up"
-                data-aos-delay="100"
-            >
-                <a href="/details.html" class="component-products d-block">
-                <div class="products-thumbnail">
+                <div class="row">
                     <div
-                    class="products-image"
-                    style="
-                        background-image: url('/images/products-digitalillustration.jpg');
-                    "
-                    ></div>
-                </div>
-                <div class="products-text">Digital Illustration</div>
-                <div class="products-price">Rp. 120.000</div>
-                </a>
-            </div>
-            <div
-                class="col-6 col-md-4 col-lg-3"
-                data-aos="fade-up"
-                data-aos-delay="200"
-            >
-                <a href="/details.html" class="component-products d-block">
-                <div class="products-thumbnail">
+                        class="col-6 col-md-4 col-lg-3"
+                        data-aos="fade-up"
+                        data-aos-delay="100"
+                    >
+                        <a href="/details.html" class="component-products d-block">
+                            <div class="products-thumbnail">
+                                <div
+                                    class="products-image"
+                                    style="background-image: url('/images/products-digitalillustration.jpg');"
+                                >
+                                </div>
+                            </div>
+                            <div class="products-text">
+                                Digital Illustration
+                            </div>
+                            <div class="products-price">
+                                Rp. 120.000
+                            </div>
+                        </a>
+                    </div>
                     <div
-                    class="products-image"
-                    style="
-                        background-image: url('/images/products-illustrationsingle.jpg');
-                    "
-                    ></div>
-                </div>
-                <div class="products-text">Illustration Single</div>
-                <div class="products-price">Rp. 100.000</div>
-                </a>
-            </div>
-            <div
-                class="col-6 col-md-4 col-lg-3"
-                data-aos="fade-up"
-                data-aos-delay="300"
-            >
-                <a href="/details.html" class="component-products d-block">
-                <div class="products-thumbnail">
+                        class="col-6 col-md-4 col-lg-3"
+                        data-aos="fade-up"
+                        data-aos-delay="200"
+                    >
+                        <a href="/details.html" class="component-products d-block">
+                            <div class="products-thumbnail">
+                                <div
+                                    class="products-image"
+                                    style="background-image: url('/images/products-illustrationsingle.jpg');"
+                                >
+                                </div>
+                            </div>
+                            <div class="products-text">
+                                Illustration Single
+                            </div>
+                            <div class="products-price">
+                                Rp. 100.000
+                            </div>
+                        </a>
+                    </div>
                     <div
-                    class="products-image"
-                    style="
-                        background-image: url('/images/products-cartoonillustration.jpg');
-                    "
-                    ></div>
-                </div>
-                <div class="products-text">Cartoon Illustration</div>
-                <div class="products-price">Rp. 120.000</div>
-                </a>
-            </div>
-            <div
-                class="col-6 col-md-4 col-lg-3"
-                data-aos="fade-up"
-                data-aos-delay="400"
-            >
-                <a href="/details.html" class="component-products d-block">
-                <div class="products-thumbnail">
+                        class="col-6 col-md-4 col-lg-3"
+                        data-aos="fade-up"
+                        data-aos-delay="300"
+                    >
+                        <a href="/details.html" class="component-products d-block">
+                            <div class="products-thumbnail">
+                                <div
+                                    class="products-image"
+                                    style="background-image: url('/images/products-cartoonillustration.jpg');"
+                                >
+                                </div>
+                            </div>
+                            <div class="products-text">
+                                Cartoon Illustration
+                            </div>
+                            <div class="products-price">
+                                Rp. 120.000
+                            </div>
+                        </a>
+                    </div>
                     <div
-                    class="products-image"
-                    style="
-                        background-image: url('/images/products-foodart.jpg');
-                    "
-                    ></div>
-                </div>
-                <div class="products-text">Food Art</div>
-                <div class="products-price">Rp. 75.000</div>
-                </a>
-            </div>
-            <div
-                class="col-6 col-md-4 col-lg-3"
-                data-aos="fade-up"
-                data-aos-delay="500"
-            >
-                <a href="/details.html" class="component-products d-block">
-                <div class="products-thumbnail">
+                        class="col-6 col-md-4 col-lg-3"
+                        data-aos="fade-up"
+                        data-aos-delay="400"
+                    >
+                        <a href="/details.html" class="component-products d-block">
+                            <div class="products-thumbnail">
+                                <div
+                                    class="products-image"
+                                    style="background-image: url('/images/products-foodart.jpg');"
+                                >
+                                </div>
+                            </div>
+                            <div class="products-text">
+                                Food Art
+                            </div>
+                            <div class="products-price">
+                                Rp. 75.000
+                            </div>
+                        </a>
+                    </div>
                     <div
-                    class="products-image"
-                    style="
-                        background-image: url('/images/products-birdontree.jpg');
-                    "
-                    ></div>
-                </div>
-                <div class="products-text">Bird on Tree</div>
-                <div class="products-price">Rp. 200.000</div>
-                </a>
-            </div>
-            <div
-                class="col-6 col-md-4 col-lg-3"
-                data-aos="fade-up"
-                data-aos-delay="600"
-            >
-                <a href="/details.html" class="component-products d-block">
-                <div class="products-thumbnail">
+                        class="col-6 col-md-4 col-lg-3"
+                        data-aos="fade-up"
+                        data-aos-delay="500"
+                    >
+                        <a href="/details.html" class="component-products d-block">
+                            <div class="products-thumbnail">
+                                <div
+                                    class="products-image"
+                                    style="background-image: url('/images/products-birdontree.jpg');"
+                                >
+                                </div>
+                            </div>
+                            <div class="products-text">
+                                Bird on Tree
+                            </div>
+                            <div class="products-price">
+                                Rp. 200.000
+                            </div>
+                        </a>
+                    </div>
                     <div
-                    class="products-image"
-                    style="
-                        background-image: url('/images/products-petaleddflower.jpg');
-                    "
-                    ></div>
-                </div>
-                <div class="products-text">Petaledd Flower</div>
-                <div class="products-price">Rp. 90.000</div>
-                </a>
-            </div>
-            <div
-                class="col-6 col-md-4 col-lg-3"
-                data-aos="fade-up"
-                data-aos-delay="700"
-            >
-                <a href="/details.html" class="component-products d-block">
-                <div class="products-thumbnail">
+                        class="col-6 col-md-4 col-lg-3"
+                        data-aos="fade-up"
+                        data-aos-delay="600"
+                    >
+                        <a href="/details.html" class="component-products d-block">
+                            <div class="products-thumbnail">
+                                <div
+                                    class="products-image"
+                                    style="background-image: url('/images/products-petaleddflower.jpg');"
+                                >
+                                </div>
+                            </div>
+                            <div class="products-text">
+                                Petaledd Flower
+                            </div>
+                            <div class="products-price">
+                                Rp. 90.000
+                            </div>
+                        </a>
+                    </div>
                     <div
-                    class="products-image"
-                    style="
-                        background-image: url('/images/products-macramewall.jpg');
-                    "
-                    ></div>
-                </div>
-                <div class="products-text">Macrame Wall Decoration</div>
-                <div class="products-price">Rp. 105.000</div>
-                </a>
-            </div>
-            <div
-                class="col-6 col-md-4 col-lg-3"
-                data-aos="fade-up"
-                data-aos-delay="800"
-            >
-                <a href="/details.html" class="component-products d-block">
-                <div class="products-thumbnail">
+                        class="col-6 col-md-4 col-lg-3"
+                        data-aos="fade-up"
+                        data-aos-delay="700"
+                    >
+                        <a href="/details.html" class="component-products d-block">
+                            <div class="products-thumbnail">
+                                <div
+                                    class="products-image"
+                                    style="background-image: url('/images/products-macramewall.jpg');"
+                                >
+                                </div>
+                            </div>
+                            <div class="products-text">
+                                Macrame Wall Decoration
+                            </div>
+                            <div class="products-price">
+                                Rp. 105.000
+                            </div>
+                        </a>
+                    </div>
                     <div
-                    class="products-image"
-                    style="
-                        background-image: url('/images/products-floraltextile.jpg');
-                    "
-                    ></div>
+                        class="col-6 col-md-4 col-lg-3"
+                        data-aos="fade-up"
+                        data-aos-delay="800"
+                    >
+                        <a href="/details.html" class="component-products d-block">
+                            <div class="products-thumbnail">
+                                <div
+                                    class="products-image"
+                                    style="background-image: url('/images/products-floraltextile.jpg');"
+                                >
+                                </div>
+                            </div>
+                            <div class="products-text">
+                                Floral Textile
+                            </div>
+                            <div class="products-price">
+                                Rp. 60.000
+                            </div>
+                        </a>
+                    </div>
                 </div>
-                <div class="products-text">Floral Textile</div>
-                <div class="products-price">Rp. 60.000</div>
-                </a>
             </div>
-            </div>
-        </div>
         </section>
     </div>
 @endsection
