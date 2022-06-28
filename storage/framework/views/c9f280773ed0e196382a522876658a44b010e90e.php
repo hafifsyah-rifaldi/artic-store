feab6a02803e885a515f70c7641935a93a65fa54
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-06-28 23:24:57 +0700

    Integrasi Register Integrasi Halaman Register

--- storage/framework/views/c9f280773ed0e196382a522876658a44b010e90e.php
+++ storage/framework/views/c9f280773ed0e196382a522876658a44b010e90e.php
@@ -2,205 +2,215 @@
 
 <div class="page-content page-auth" id="register">
     <div class="section-store-auth" data-aos="fade-up">
-    <div class="container">
-        <div class="row align-items-center justify-content-center row-login">
-        <div class="col-lg-4">
-            <h2>
-            Memulai dengan ARTiC <br />
-            Buat Akun
-            </h2>
-            <form class="mt-3">
-            <div class="form-group">
-                <label>Nama Lengkap</label>
-                <input
-                type="text"
-                class="form-control is-valid"
-                v-model="name"
-                autofocus
-                />
-            </div>
-            <div class="form-group">
-                <label>E-mail</label>
-                <input
-                type="email"
-                class="form-control is-invalid"
-                v-model="email"
-                />
-            </div>
-            <div class="form-group">
-                <label>Password</label>
-                <input type="password" class="form-control" />
-            </div>
-            <div class="form-group">
-                <label>ARTiC Store</label>
-                <p class="text-muted">Apakah anda ingin membuka store?</p>
-                <div
-                class="custom-control custom-radio custom-control-inline"
-                >
-                <input
-                    type="radio"
-                    class="custom-control-input"
-                    name="is_store_open"
-                    id="openStoreTrue"
-                    v-model="is_store_open"
-                    :value="true"
-                />
-                <label for="openStoreTrue" class="custom-control-label">
-                    Iya
-                </label>
-                </div>
-                <div
-                class="custom-control custom-radio custom-control-inline"
-                >
-                <input
-                    type="radio"
-                    class="custom-control-input"
-                    name="is_store_open"
-                    id="openStoreFalse"
-                    v-model="is_store_open"
-                    :value="false"
-                />
-                <label for="openStoreFalse" class="custom-control-label">
-                    Tidak
-                </label>
-                </div>
-            </div>
-            <div class="form-group" v-if="is_store_open">
-                <label>Nama Store</label>
-                <input type="text" class="form-control" />
-            </div>
-            <div class="form-group" v-if="is_store_open">
-                <label>Kategori</label>
-                <select name="category" class="form-control">
-                <option value="" disabled>Select Category</option>
-                </select>
-            </div>
-            <a
-                href="/dashboard.html"
-                class="btn btn-success btn-block mt-4"
-            >
-                Sign Up Now
-            </a>
-            <a href="/login.html" class="btn btn-signup btn-block mt-2">
-                Back to Login
-            </a>
-            </form>
-        </div>
-        </div>
-    </div>
-    </div>
-</div>
-
-<div class="container" style="display: none">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header"><?php echo e(__('Register')); ?></div>
-
-                <div class="card-body">
-                    <form method="POST" action="<?php echo e(route('register')); ?>">
+        <div class="container">
+            <div class="row align-items-center justify-content-center row-login">
+                <div class="col-lg-4">
+                    <h2>
+                        Memulai dengan ARTiC <br />
+                        Buat Akun
+                    </h2>
+                     <form method="POST" action="<?php echo e(route('register')); ?>">
                         <?php echo csrf_field(); ?>
-
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end"><?php echo e(__('Name')); ?></label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control <?php $__errorArgs = ['name'];
+                        <div class="form-group">
+                            <label>Nama Lengkap</label>
+                            <input id="name" 
+                                v-model="name"
+                                type="text" 
+                                class="form-control <?php $__errorArgs = ['name'];
 $__bag = $errors->getBag($__errorArgs[1] ?? 'default');
 if ($__bag->has($__errorArgs[0])) :
 if (isset($message)) { $__messageOriginal = $message; }
 $message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
 if (isset($__messageOriginal)) { $message = $__messageOriginal; }
 endif;
-unset($__errorArgs, $__bag); ?>" name="name" value="<?php echo e(old('name')); ?>" required autocomplete="name" autofocus>
-
-                                <?php $__errorArgs = ['name'];
+unset($__errorArgs, $__bag); ?>" 
+                                name="name" 
+                                value="<?php echo e(old('name')); ?>" 
+                                required 
+                                autocomplete="name" 
+                                autofocus>
+                            <?php $__errorArgs = ['name'];
 $__bag = $errors->getBag($__errorArgs[1] ?? 'default');
 if ($__bag->has($__errorArgs[0])) :
 if (isset($message)) { $__messageOriginal = $message; }
 $message = $__bag->first($__errorArgs[0]); ?>
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong><?php echo e($message); ?></strong>
-                                    </span>
-                                <?php unset($message);
+                                <span class="invalid-feedback" role="alert">
+                                    <strong><?php echo e($message); ?></strong>
+                                </span>
+                            <?php unset($message);
 if (isset($__messageOriginal)) { $message = $__messageOriginal; }
 endif;
 unset($__errorArgs, $__bag); ?>
-                            </div>
                         </div>
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end"><?php echo e(__('Email Address')); ?></label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control <?php $__errorArgs = ['email'];
+                        <div class="form-group">
+                            <label>E-mail</label>
+                            <input id="email"
+                                v-model="email"
+                                type="email" 
+                                class="form-control <?php $__errorArgs = ['email'];
 $__bag = $errors->getBag($__errorArgs[1] ?? 'default');
 if ($__bag->has($__errorArgs[0])) :
 if (isset($message)) { $__messageOriginal = $message; }
 $message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
 if (isset($__messageOriginal)) { $message = $__messageOriginal; }
 endif;
-unset($__errorArgs, $__bag); ?>" name="email" value="<?php echo e(old('email')); ?>" required autocomplete="email">
-
-                                <?php $__errorArgs = ['email'];
+unset($__errorArgs, $__bag); ?>" 
+                                name="email" value="<?php echo e(old('email')); ?>" 
+                                required 
+                                autocomplete="email">
+                            <?php $__errorArgs = ['email'];
 $__bag = $errors->getBag($__errorArgs[1] ?? 'default');
 if ($__bag->has($__errorArgs[0])) :
 if (isset($message)) { $__messageOriginal = $message; }
 $message = $__bag->first($__errorArgs[0]); ?>
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong><?php echo e($message); ?></strong>
-                                    </span>
-                                <?php unset($message);
+                                <span class="invalid-feedback" role="alert">
+                                    <strong><?php echo e($message); ?></strong>
+                                </span>
+                            <?php unset($message);
 if (isset($__messageOriginal)) { $message = $__messageOriginal; }
 endif;
 unset($__errorArgs, $__bag); ?>
-                            </div>
                         </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end"><?php echo e(__('Password')); ?></label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control <?php $__errorArgs = ['password'];
+                        <div class="form-group">
+                            <label>Password</label>
+                            <input id="password" 
+                                type="password" 
+                                class="form-control <?php $__errorArgs = ['password'];
 $__bag = $errors->getBag($__errorArgs[1] ?? 'default');
 if ($__bag->has($__errorArgs[0])) :
 if (isset($message)) { $__messageOriginal = $message; }
 $message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
 if (isset($__messageOriginal)) { $message = $__messageOriginal; }
 endif;
-unset($__errorArgs, $__bag); ?>" name="password" required autocomplete="new-password">
-
-                                <?php $__errorArgs = ['password'];
+unset($__errorArgs, $__bag); ?>" 
+                                name="password" 
+                                required 
+                                autocomplete="new-password">
+                            <?php $__errorArgs = ['password'];
 $__bag = $errors->getBag($__errorArgs[1] ?? 'default');
 if ($__bag->has($__errorArgs[0])) :
 if (isset($message)) { $__messageOriginal = $message; }
 $message = $__bag->first($__errorArgs[0]); ?>
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong><?php echo e($message); ?></strong>
-                                    </span>
-                                <?php unset($message);
+                                <span class="invalid-feedback" role="alert">
+                                    <strong><?php echo e($message); ?></strong>
+                                </span>
+                            <?php unset($message);
 if (isset($__messageOriginal)) { $message = $__messageOriginal; }
 endif;
 unset($__errorArgs, $__bag); ?>
+                        </div>
+                        <div class="form-group">
+                            <label>Confirm Password</label>
+                            <input id="password-confirm" 
+                                type="password" 
+                                class="form-control <?php $__errorArgs = ['password_confirm'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>" 
+                                name="password_confirm" 
+                                required 
+                                autocomplete="new-password">
+                            <?php $__errorArgs = ['password_confirm'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong><?php echo e($message); ?></strong>
+                                </span>
+                            <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                        </div>
+                        <div class="form-group">
+                            <label>ARTiC Store</label>
+                            <p class="text-muted">
+                                Apakah anda ingin membuka store?
+                            </p>
+                            <div
+                                class="custom-control custom-radio custom-control-inline"
+                            >
+                                <input
+                                    type="radio"
+                                    class="custom-control-input"
+                                    name="is_store_open"
+                                    id="openStoreTrue"
+                                    v-model="is_store_open"
+                                    :value="true"
+                                />
+                                <label for="openStoreTrue" class="custom-control-label">
+                                    Iya
+                                </label>
+                            </div>
+                            <div
+                            class="custom-control custom-radio custom-control-inline"
+                            >
+                                <input
+                                    type="radio"
+                                    class="custom-control-input"
+                                    name="is_store_open"
+                                    id="openStoreFalse"
+                                    v-model="is_store_open"
+                                    :value="false"
+                                />
+                                <label for="openStoreFalse" class="custom-control-label">
+                                    Tidak
+                                </label>
                             </div>
                         </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end"><?php echo e(__('Confirm Password')); ?></label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
+                        <div class="form-group" v-if="is_store_open">
+                            <label>Nama Store</label>
+                            <input type="text"
+                                v-model="store_name"
+                                id="store_name"
+                                class="form-control <?php $__errorArgs = ['password_confirm'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>"
+                                name="store_name"
+                                required
+                                autocomplete
+                                autofocus>
+                            <?php $__errorArgs = ['store_name'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong><?php echo e($message); ?></strong>
+                                </span>
+                            <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
                         </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    <?php echo e(__('Register')); ?>
-
-                                </button>
-                            </div>
+                        <div class="form-group" v-if="is_store_open">
+                            <label>Kategori</label>
+                            <select name="category" class="form-control">
+                                <option value="" disabled>Select Category</option>
+                                <?php $__currentLoopData = $categories; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $category): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                    <option value="<?php echo e($category->id); ?>"><?php echo e($category->name); ?></option>
+                                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                            </select>
                         </div>
+                        <button
+                            type="submit"
+                            class="btn btn-success btn-block mt-4"
+                        >
+                            Sign Up Now
+                        </button>
+                        <a href="<?php echo e(route('login')); ?>" class="btn btn-signup btn-block mt-2">
+                            Back to Login
+                        </a>
                     </form>
                 </div>
             </div>
@@ -219,14 +229,14 @@ unset($__errorArgs, $__bag); ?>
         el: "#register",
         mounted() {
           AOS.init();
-          this.$toasted.error(
-            "Maaf, tampaknya email sudah terdaftar pada sistem kami.",
-            {
-              position: "top-center",
-              className: "rounded",
-              duration: 1000,
-            }
-          );
+            this.$toasted.error(
+                "Maaf, tampaknya email sudah terdaftar pada sistem kami.",
+                {
+                position: "top-center",
+                className: "rounded",
+                duration: 1000,
+                }
+            );
         },
         data: {
           name: "Hafifsyah Rifaldi",
