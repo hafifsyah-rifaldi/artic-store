ac2b4e8275bbcdaa1d07722bb401ada1c5f06150
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-06-29 22:00:12 +0700

    Integrasi Modul Checkout Mengubah Model Transaction

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,10 +2,29 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'users_id',
+        'insurance_price',
+        'shipping_price',
+        'total_price',
+        'code',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        
+    ];
 }
