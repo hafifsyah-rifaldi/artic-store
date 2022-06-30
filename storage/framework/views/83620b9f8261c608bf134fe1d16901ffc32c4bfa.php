194f1b1fcc27294db1f4596d4b12cfecc3390acf
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-06-30 21:40:23 +0700

    Integrasi Dashboard Page Integrasi View

--- storage/framework/views/83620b9f8261c608bf134fe1d16901ffc32c4bfa.php
+++ storage/framework/views/83620b9f8261c608bf134fe1d16901ffc32c4bfa.php
@@ -7,121 +7,103 @@
 <?php $__env->startSection('content'); ?>
 <!-- Section Content -->
 <div
-class="section-content section-dashboard-home"
-data-aos="fade-up"
+    class="section-content section-dashboard-home"
+    data-aos="fade-up"
 >
-<div class="container-fluid">
-    <div class="dashboard-heading">
-    <h2 class="dashboard-title">Dashboard</h2>
-    <p class="dashboard-subtitle">Look what you have made today!</p>
-    </div>
+    <div class="container-fluid">
+        <div class="dashboard-heading">
+            <h2 class="dashboard-title">
+                Dashboard
+            </h2>
+            <p class="dashboard-subtitle">
+                Look what you have made today!
+            </p>
+        </div>
 
-    <div class="dashboard-content">
-    <div class="row">
-        <div class="col-md-4">
-        <div class="card mb-2">
-            <div class="card-body">
-            <div class="dashboard-card-title">Customer</div>
-            <div class="dashboard-card-subtitle">15</div>
+        <div class="dashboard-content">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card mb-2">
+                        <div class="card-body">
+                            <div class="dashboard-card-title">
+                                Customer
+                            </div>
+                            <div class="dashboard-card-subtitle">
+                                <?php echo e(number_format($customer)); ?>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card mb-2">
+                        <div class="card-body">
+                            <div class="dashboard-card-title">
+                                Revenue
+                            </div>
+                            <div class="dashboard-card-subtitle">
+                                Rp. <?php echo number_format($revenue,0,',','.'); ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card mb-2">
+                        <div class="card-body">
+                            <div class="dashboard-card-title">
+                                Transaction
+                            </div>
+                            <div class="dashboard-card-subtitle">
+                               <?php echo e(number_format($customer)); ?>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-        </div>
-        <div class="col-md-4">
-        <div class="card mb-2">
-            <div class="card-body">
-            <div class="dashboard-card-title">Revenue</div>
-            <div class="dashboard-card-subtitle">Rp. 450.000</div>
+
+            <div class="row mt-3">
+                <div class="col-12 mt-2">
+                    <h5 class="mb-3">Recent Transactions</h5>
+                    <?php $__currentLoopData = $transaction_data; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $transaction): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                        <a
+                            href="<?php echo e(route('dashboard-transaction-details', $transaction->id)); ?>"
+                            class="card card-list d-block"
+                        >
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-1">
+                                        <img
+                                            src="<?php echo e(Storage::url($transaction->product->galleries->first()->photos ?? '')); ?>"
+                                            alt=""
+                                        />
+                                    </div>
+                                    <div class="col-md-4">
+                                        <?php echo e($transaction->product->name ?? ''); ?>
+
+                                    </div>
+                                    <div class="col-md-3">
+                                        <?php echo e($transaction->transaction->user->name ?? ''); ?>
+
+                                    </div>
+                                    <div class="col-md-3">
+                                        <?php echo e($transaction->created_at ?? ''); ?>
+
+                                    </div>
+                                    <div class="col-md-1 d-none d-md-block">
+                                        <img
+                                            src="/images/dashboard-arrow-right.svg"
+                                            alt=""
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                </div>
             </div>
-        </div>
-        </div>
-        <div class="col-md-4">
-        <div class="card mb-2">
-            <div class="card-body">
-            <div class="dashboard-card-title">Transaction</div>
-            <div class="dashboard-card-subtitle">15</div>
-            </div>
-        </div>
         </div>
     </div>
-    <div class="row mt-3">
-        <div class="col-12 mt-2">
-        <h5 class="mb-3">Recent Transactions</h5>
-        <a
-            href="/dashboard-transactions-details.html"
-            class="card card-list d-block"
-        >
-            <div class="card-body">
-            <div class="row">
-                <div class="col-md-1">
-                <img
-                    src="/images/dashboard-icon-product-1.png"
-                    alt=""
-                />
-                </div>
-                <div class="col-md-4">Bird On Tree</div>
-                <div class="col-md-3">Mc Gill</div>
-                <div class="col-md-3">25 Maret, 2022</div>
-                <div class="col-md-1 d-none d-md-block">
-                <img
-                    src="/images/dashboard-arrow-right.svg"
-                    alt=""
-                />
-                </div>
-            </div>
-            </div>
-        </a>
-        <a
-            href="/dashboard-transactions-details.html"
-            class="card card-list d-block"
-        >
-            <div class="card-body">
-            <div class="row">
-                <div class="col-md-1">
-                <img
-                    src="/images/dashboard-icon-product-2.png"
-                    alt=""
-                />
-                </div>
-                <div class="col-md-4">Cartoon Illustration</div>
-                <div class="col-md-3">Salsa Tifani</div>
-                <div class="col-md-3">26 Maret, 2022</div>
-                <div class="col-md-1 d-none d-md-block">
-                <img
-                    src="/images/dashboard-arrow-right.svg"
-                    alt=""
-                />
-                </div>
-            </div>
-            </div>
-        </a>
-        <a
-            href="/dashboard-transactions-details.html"
-            class="card card-list d-block"
-        >
-            <div class="card-body">
-            <div class="row">
-                <div class="col-md-1">
-                <img
-                    src="/images/dashboard-icon-product-3.png"
-                    alt=""
-                />
-                </div>
-                <div class="col-md-4">Digital Illustration</div>
-                <div class="col-md-3">Salsa Tifani</div>
-                <div class="col-md-3">26 Maret, 2022</div>
-                <div class="col-md-1 d-none d-md-block">
-                <img
-                    src="/images/dashboard-arrow-right.svg"
-                    alt=""
-                />
-                </div>
-            </div>
-            </div>
-        </a>
-        </div>
-    </div>
-    </div>
-</div>
 </div>
 <?php $__env->stopSection(); ?>
 <?php echo $__env->make('layouts.dashboard', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\laragon\www\artic-store\resources\views/pages/dashboard.blade.php ENDPATH**/ ?>
