64863a38f0ca694faa903ccd453aa476324b2d72
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-06-30 23:42:32 +0700

    Integrasi Dashboard My Product Detail dan Edit

--- storage/framework/views/595e2c9308e78dcc01e35b6fff2c3755c4912eda.php
+++ storage/framework/views/595e2c9308e78dcc01e35b6fff2c3755c4912eda.php
@@ -18,7 +18,18 @@
         <div class="dashboard-content">
         <div class="row">
             <div class="col-12">
-            <form action="">
+                <?php if($errors->any()): ?>
+                    <div class="alert alert-danger">
+                        <ul>
+                            <?php $__currentLoopData = $errors->all(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $error): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                <li><?php echo e($error); ?></li>
+                            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                        </ul>
+                    </div>
+                <?php endif; ?>
+            <form action="<?php echo e(route('dashboard-product-update', $product->id)); ?>" method="POST" enctype="multipart/form-data">
+                <?php echo csrf_field(); ?>
+                <input type="hidden" name="users_id" value="<?php echo e(Auth::user()->id); ?>">
                 <div class="card">
                 <div class="card-body">
                     <div class="row">
@@ -27,8 +38,10 @@
                         <label>Product Name</label>
                         <input
                             type="text"
+                            name="name"
                             class="form-control"
                             value="Abstract Painting"
+                            value="<?php echo e($product->name); ?>"
                         />
                         </div>
                     </div>
@@ -37,25 +50,28 @@
                         <label>Price</label>
                         <input
                             type="number"
+                            name="price"
                             class="form-control"
                             value="200"
+                            value="<?php echo e($product->price); ?>"
                         />
                         </div>
                     </div>
                     <div class="col-md-12">
                         <div class="form-group">
                         <label>Kategori</label>
-                        <select name="category" class="form-control">
-                            <option value="" disabled>
-                            Select Category
-                            </option>
+                        <select name="categories_id" class="form-control">
+                            <option value="<?php echo e($product->categories_id); ?>">Tidak diganti (<?php echo e($product->category->name); ?>)</option>
+                            <?php $__currentLoopData = $categories; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $category): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                <option value="<?php echo e($category->id); ?>"><?php echo e($category->name); ?></option>
+                            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
                         </select>
                         </div>
                     </div>
                     <div class="col-md-12">
                         <div class="form-group">
                         <label>Description</label>
-                        <textarea name="editor"></textarea>
+                        <textarea name="description" id="editor"><?php echo $product->description; ?></textarea>
                         </div>
                     </div>
                     </div>
@@ -80,55 +96,39 @@
             <div class="card">
             <div class="card-body">
                 <div class="row">
+                 <?php $__currentLoopData = $product->galleries; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $gallery): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
                 <div class="col-md-4">
                     <div class="gallery-container">
                     <img
-                        src="/images/product-card-1.png"
+                        src="<?php echo e(Storage::url($gallery->photos ?? '')); ?>"
                         alt=""
                         class="w-100"
                     />
-                    <a href="#" class="delete-gallery">
+                    <a href="<?php echo e(route('dashboard-product-gallery-delete', $gallery->id)); ?>" class="delete-gallery">
                         <img src="/images/icon-delete.svg" alt="" />
                     </a>
                     </div>
                 </div>
-                <div class="col-md-4">
-                    <div class="gallery-container">
-                    <img
-                        src="/images/product-card-2.png"
-                        alt=""
-                        class="w-100"
-                    />
-                    <a href="#" class="delete-gallery">
-                        <img src="/images/icon-delete.svg" alt="" />
-                    </a>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="gallery-container">
-                    <img
-                        src="/images/product-card-1.png"
-                        alt=""
-                        class="w-100"
-                    />
-                    <a href="#" class="delete-gallery">
-                        <img src="/images/icon-delete.svg" alt="" />
-                    </a>
-                    </div>
-                </div>
+                 <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
                 <div class="col-12">
-                    <input
-                    type="file"
-                    id="file"
-                    style="display: none"
-                    multiple
-                    />
-                    <button
-                    class="btn btn-secondary btn-block mt-3"
-                    onclick="thisFileUpload()"
-                    >
-                    Add Photo
-                    </button>
+                   <form action="<?php echo e(route('dashboard-product-gallery-upload')); ?>" method="POST" enctype="multipart/form-data">
+                    <?php echo csrf_field(); ?>
+                    <input type="hidden" name="products_id" value="<?php echo e($product->id); ?>">
+                     <input
+                        type="file"
+                        name="photos"
+                        id="file"
+                        style="display: none"
+                        onchange="form.submit()"
+                        />
+                        <button
+                        type="button"
+                        class="btn btn-secondary btn-block mt-3"
+                        onclick="thisFileUpload()"
+                        >
+                        Add Photo
+                        </button>
+                   </form>
                 </div>
                 </div>
             </div>
