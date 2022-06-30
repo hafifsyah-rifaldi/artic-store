e8a39e85fc5db00f79f4cbe33f7c10ed5418dc99
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-06-30 22:35:23 +0700

    Integrasi Dashboard My Product Index Page

--- storage/framework/views/a4bcfbb33c636f16dfb649c54d07569ed55607b7.php
+++ storage/framework/views/a4bcfbb33c636f16dfb649c54d07569ed55607b7.php
@@ -7,111 +7,49 @@
 <?php $__env->startSection('content'); ?>
      <!-- Section Content -->
     <div
-    class="section-content section-dashboard-home"
-    data-aos="fade-up"
+        class="section-content section-dashboard-home"
+        data-aos="fade-up"
     >
-    <div class="container-fluid">
-        <div class="dashboard-heading">
-        <h2 class="dashboard-title">My Products</h2>
-        <p class="dashboard-subtitle">
-            Manage it well and be a creative enthusiasts!
-        </p>
-        </div>
+        <div class="container-fluid">
+            <div class="dashboard-heading">
+                <h2 class="dashboard-title">My Products</h2>
+                <p class="dashboard-subtitle">
+                    Manage it well and be a creative enthusiasts!
+                </p>
+            </div>
 
-        <div class="dashboard-content">
-        <div class="row">
-            <div class="col-12">
-            <a
-                href="/dashboard-products-create.html"
-                class="btn btn-success"
-                >Add New Products</a
-            >
+            <div class="dashboard-content">
+                <div class="row">
+                    <div class="col-12">
+                    <a
+                        href="<?php echo e(route('dashboard-product-create')); ?>"
+                        class="btn btn-success"
+                        >Add New Products</a
+                    >
+                    </div>
+                </div>
+                <div class="row mt-4">
+                    <?php $__currentLoopData = $products; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $product): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                        <a
+                            href="<?php echo e(route('dashboard-product-details', $product->id)); ?>"
+                            class="card card-dashboard-product d-block"
+                        >
+                            <div class="card-body">
+                                <img
+                                    src="<?php echo e(Storage::url($product->galleries->first()->photos ?? '')); ?>"
+                                    alt=""
+                                    class="w-100 mb-2"
+                                />
+                                <div class="product-title"><?php echo e($product->name); ?></div>
+                                <div class="product-category"><?php echo e($product->category->name); ?></div>
+                            </div>
+                        </a>
+                    </div> 
+                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                </div>
             </div>
         </div>
-        <div class="row mt-4">
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-            <a
-                href="/dashboard-products-details.html"
-                class="card card-dashboard-product d-block"
-            >
-                <div class="card-body">
-                <img
-                    src="/images/product-card-1.png"
-                    alt=""
-                    class="w-100 mb-2"
-                />
-                <div class="product-title">Abstract Painting</div>
-                <div class="product-category">Art Painting</div>
-                </div>
-            </a>
-            </div>
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-            <a
-                href="/dashboard-products-details.html"
-                class="card card-dashboard-product d-block"
-            >
-                <div class="card-body">
-                <img
-                    src="/images/product-card-2.png"
-                    alt=""
-                    class="w-100 mb-2"
-                />
-                <div class="product-title">Art Abstract</div>
-                <div class="product-category">Art Painting</div>
-                </div>
-            </a>
-            </div>
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-            <a
-                href="/dashboard-products-details.html"
-                class="card card-dashboard-product d-block"
-            >
-                <div class="card-body">
-                <img
-                    src="/images/product-card-3.png"
-                    alt=""
-                    class="w-100 mb-2"
-                />
-                <div class="product-title">Abstract Painting</div>
-                <div class="product-category">Art Painting</div>
-                </div>
-            </a>
-            </div>
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-            <a
-                href="/dashboard-products-details.html"
-                class="card card-dashboard-product d-block"
-            >
-                <div class="card-body">
-                <img
-                    src="/images/product-card-4.png"
-                    alt=""
-                    class="w-100 mb-2"
-                />
-                <div class="product-title">Art Abstract</div>
-                <div class="product-category">Art Painting</div>
-                </div>
-            </a>
-            </div>
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-            <a
-                href="/dashboard-products-details.html"
-                class="card card-dashboard-product d-block"
-            >
-                <div class="card-body">
-                <img
-                    src="/images/product-card-5.png"
-                    alt=""
-                    class="w-100 mb-2"
-                />
-                <div class="product-title">Abstract Painting</div>
-                <div class="product-category">Art Painting</div>
-                </div>
-            </a>
-            </div>
-        </div>
-        </div>
-    </div>
     </div>
 <?php $__env->stopSection(); ?>
 <?php echo $__env->make('layouts.dashboard', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\laragon\www\artic-store\resources\views/pages/dashboard-products.blade.php ENDPATH**/ ?>
