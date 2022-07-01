43a9603c6409b285c962808d9781e0502dee0bfb
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-07-01 21:37:13 +0700

    Membuat Halaman Statistik

--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -30,7 +30,7 @@ data-aos="fade-up"
         <div class="card mb-2">
             <div class="card-body">
             <div class="dashboard-card-title">Revenue</div>
-            <div class="dashboard-card-subtitle">Rp. {{ $revenue }} </div>
+            <div class="dashboard-card-subtitle">@currency($revenue)</div>
             </div>
         </div>
         </div>
